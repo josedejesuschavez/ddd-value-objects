356edd917e7bd769a4e94659b57debc211dacfcb
José de Jesús Chavez Ascencio <chavezjal06@hotmail.com>
2026-02-21 17:52:16 -0600

    --

--- src/ddd_value_objects/string_value_object.py
+++ src/ddd_value_objects/string_value_object.py
@@ -1,15 +1,15 @@
+from dataclasses import dataclass
+
 from .value_object import ValueObject, Primitives
 
 
+@dataclass(frozen=True, slots=True)
 class StringValueObject(ValueObject[str]):
+    def __post_init__(self):
+        super().__post_init__()
+        self._ensure_value_is_string(self.value)
 
-    def __init__(self, value: str):
-        super().__init__(value)
-
-    def equals(self, other: 'ValueObject') -> bool:
-        if not isinstance(other, StringValueObject):
-            return False
-        return self.value == other.value
-
-    def __repr__(self):
-        return f"StringValueObject(value='{self.value}')"
+    @staticmethod
+    def _ensure_value_is_string(value) -> None:
+        if not isinstance(value, str):
+            raise TypeError(f"Value must be a string, got {type(value)}")

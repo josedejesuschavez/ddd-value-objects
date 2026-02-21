db0f443cb4b121f463b2553b2dc4655520cb1041
José de Jesús Chavez Ascencio <chavezjal06@hotmail.com>
2026-02-21 17:40:17 -0600

    --

--- src/ddd_value_objects/country_code_value_object.py
+++ src/ddd_value_objects/country_code_value_object.py
@@ -1,23 +1,23 @@
 import re
+from dataclasses import dataclass
 
 from .invalid_argument_error import InvalidArgumentError
 from .string_value_object import StringValueObject
 
 
+@dataclass(frozen=True, slots=True)
 class CountryCodeValueObject(StringValueObject):
     """
     Value Object for ISO 3166-1 alpha-2 country codes.
     """
 
-    def __init__(self, value: str):
-        super().__init__(value)
-        self._ensure_is_valid_country_code(value)
+    def __post_init__(self):
+        super().__post_init__()
+        self._ensure_is_valid_country_code(self.value)
 
-    def _ensure_is_valid_country_code(self, value: str) -> None:
+    @staticmethod
+    def _ensure_is_valid_country_code(value: str) -> None:
         if not re.match(r"^[A-Z]{2}$", value):
             raise InvalidArgumentError(
                 f"'{value}' is not a valid ISO 3166-1 alpha-2 country code"
             )
-
-    def __repr__(self):
-        return f"CountryCodeValueObject(value='{self.value}')"
